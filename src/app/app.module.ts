2b39c36c774e4d3c68be8f20bfa0dce25598c5d9
Claudio Stuardo <claudio.stuardo96@gmail.com>
2019-12-15 23:59:05 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Para usar [(ngModel)]
+import { FormsModule } from '@angular/forms';
+
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+ // Sockets
+ // npm i ngx-socket-io
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { FooterComponent } from './components/footer/footer.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MensajesComponent } from './pages/mensajes/mensajes.component';
+
+const config: SocketIoConfig = {
+   url: environment.wsUrl, options: {}
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ChatComponent,
+    ListaUsuariosComponent,
+    LoginComponent,
+    MensajesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot(config),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/websocket.service.ts
@@ -0,0 +1,102 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Usuario } from '../models/usuario.models';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  public socketStatus = false;
+  public usuario: Usuario;
+
+  constructor(
+    private socket: Socket,
+    private router: Router
+  ) {
+    this.cargarStorage();
+    this.checkStatus();
+  }
+
+  checkStatus() {
+
+    this.socket.on('connect', () => {
+
+      console.log('Conectado al servidor');
+      this.socketStatus = true;
+      this.cargarStorage();
+
+    });
+
+    this.socket.on('disconnect', () => {
+
+      console.log('Desconectado del servidor');
+      this.socketStatus = false;
+
+    });
+
+  }
+
+  emit( evento: string, payload?: any, callback?: any ) {
+
+    this.socket.emit( evento, payload, callback );
+
+  }
+
+  // Recibe el evento y lo transforma en un observable
+  listen( evento: string ) {
+
+    return this.socket.fromEvent( evento );
+
+  }
+
+  loginWS( nombre: string ) {
+
+    // console.log('Configurando: ' + nombre);
+    return new Promise( (resolve, reject) => {
+
+      this.emit( 'configurar-usuario', { nombre }, ( resp: any ) => {
+
+        this.usuario = new Usuario( nombre );
+        this.guardarStorage();
+        resolve();
+
+      });
+
+    });
+
+  }
+
+  logoutWS() {
+
+    this.usuario = null;
+    localStorage.removeItem('usuario');
+    const payload = {
+      nombre: 'sin-nombre'
+    };
+    this.emit( 'configurar-usuario', payload, () => {} );
+    this.router.navigateByUrl('');
+
+  }
+
+  getUsuario() {
+    return this.usuario;
+  }
+
+  guardarStorage() {
+
+    localStorage.setItem( 'usuario', JSON.stringify( this.usuario ) );
+
+  }
+
+  cargarStorage() {
+
+    if ( localStorage.getItem('usuario') ) {
+      this.usuario = JSON.parse( localStorage.getItem('usuario') );
+      this.loginWS( this.usuario.nombre );
+    }
+
+  }
+
+}
